f266bac18ababedce22e7a831d8622bad53e04aa
Eero12 <eerooja18@gmail.com>
2022-11-25 14:43:16 +0200

    edntiää

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -3,23 +3,15 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     public bool canMove = true;
-
     public CharacterController controller;
-
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     Vector3 velocity;
     bool isGrounded;
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (canMove)
@@ -30,21 +22,15 @@ public class Playermovement : MonoBehaviour
             {
                 velocity.y = -2f;
             }
-
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
-
             Vector3 move = transform.right * x + transform.forward * z;
-
             controller.Move(move * speed * Time.deltaTime);
-
             if (Input.GetButtonDown("Jump") && isGrounded)
             {
                 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             }
-
             velocity.y += gravity * Time.deltaTime;
-
             controller.Move(velocity * Time.deltaTime);
         }
     }
--- Assets/Scripts/followPoints.cs
+++ Assets/Scripts/followPoints.cs
@@ -6,29 +6,22 @@ public class followPoints : MonoBehaviour
 {
     [SerializeField]
     private Transform[] waypoints;
-
     [SerializeField]
     private float moveSpeed = 2f;
-
     public int waypointIndex = 0;
-
     private void Start()
     {
         transform.position = waypoints[waypointIndex].transform.position;
     }
-
-    // Update is called once per frame
     private void Update()
     {
         Move();
     }
-
     private void Move()
     {
          transform.position = Vector3.MoveTowards(transform.position,
          waypoints[waypointIndex].transform.position,
          moveSpeed * Time.deltaTime);
-
         if (transform.position == waypoints[waypointIndex].transform.position)
         {
            waypointIndex += 1;
